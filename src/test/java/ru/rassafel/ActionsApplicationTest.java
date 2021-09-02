a5ab6fd3b1fe78b3ba73b966a8a5d63de2deaac3
rassafel <rassafel@gmail.com>
2021-09-02 18:42:15 +0400

    Created tests.

---
+++ src/test/java/ru/rassafel/ActionsApplicationTest.java
@@ -0,0 +1,14 @@
+package ru.rassafel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @author rassafel
+ */
+@SpringBootTest
+class ActionsApplicationTest {
+    @Test
+    void contextLoads() {
+    }
+}
---
+++ src/test/java/ru/rassafel/HelloControllerTest.java
@@ -0,0 +1,30 @@
+package ru.rassafel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author rassafel
+ */
+@AutoConfigureWebTestClient
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class HelloControllerTest {
+    @Autowired
+    private WebTestClient webTestClient;
+
+    @Test
+    void hello() {
+        webTestClient.get()
+            .uri("/")
+            .exchange()
+            .expectStatus()
+            .isOk()
+            .expectBody(String.class)
+            .isEqualTo("Hello World!");
+    }
+}

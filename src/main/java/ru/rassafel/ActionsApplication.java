6103f8a005003ea8451401ed41fb48f977b91d94
rassafel <rassafel@gmail.com>
2021-09-02 18:41:53 +0400

    Created ActionsApplication.

---
+++ src/main/java/ru/rassafel/ActionsApplication.java
@@ -0,0 +1,14 @@
+package ru.rassafel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author rassafel
+ */
+@SpringBootApplication
+public class ActionsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ActionsApplication.class, args);
+    }
+}
